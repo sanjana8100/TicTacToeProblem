3afcbffa6ab1cb17e905907c04120eab061f7de2
sanjana8100 <sanjanams812@gmail.com>
2023-03-10 12:51:23 +0530

    added: UC1

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,8 +1,38 @@
 package com.bridgelabz;
-
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class TicTacToeGame {
+    public static char[] board= new char[10];
+
+    public static void getBoard() {
+        Arrays.fill(board, ' ');
+    }
+
+    public static void displayBoard() {
+        System.out.println("---------------");
+        for(int i=1;i<board.length;i++) {
+            if (i == 4 || i == 7) {
+                System.out.println();
+                System.out.println("---------------");
+            }
+            System.out.print("| "+board[i]+" |");
+        }
+        System.out.println();
+        System.out.println("---------------");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to TIC TAC TOE Game");
+        System.out.println("     Welcome to TIC TAC TOE Game");
+        System.out.println("****************************************");
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("To Start: PRESS 1");
+        int choice = in.nextInt();
+        if(choice ==1){
+            getBoard();
+            displayBoard();
+        }
     }
 }
