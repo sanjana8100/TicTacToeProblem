370098b9331d320b751e15ec80a231c0a8fb9625
sanjana8100 <sanjanams812@gmail.com>
2023-03-10 18:11:11 +0530

    added: UC7

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
     public static char[] board= new char[10];
-    public static char choice,compLetter,lastPlayer;
+    public static char choice,compLetter,winner='N';
 
     public static void getBoard() {
         Arrays.fill(board, ' ');
@@ -58,7 +58,7 @@ public class TicTacToeGame {
         }
     }
 
-    public static boolean Toss(){
+    public static char Toss(){
         Scanner in = new Scanner(System.in);
         System.out.println(" Tossing a coin to determine who plays first...");
         System.out.println("Please make a choice:\n1.Heads\t2.Tails");
@@ -66,14 +66,34 @@ public class TicTacToeGame {
         double toss= Math.random();
         if((toss<0.5 && userChoice==1)||(toss>=0.5 && userChoice==2)) {
             System.out.println("You won the toss and the game begins with your move!");
-            return true;
+            return choice;
         }
         else {
             System.out.println("Computer won the toss and the game begins with Computer's move!");
-            return false;
+            return compLetter;
         }
     }
 
+    public static char checkWinner(){
+        if((Character.valueOf(board[1]))==(Character.valueOf(board[2])) && (Character.valueOf(board[2]))==(Character.valueOf(board[3])) && board[2]!=' ')
+            return board[1];
+        else if((Character.valueOf(board[4]))==(Character.valueOf(board[5])) && (Character.valueOf(board[5]))==(Character.valueOf(board[6])) && board[5]!=' ')
+            return board[4];
+        else if((Character.valueOf(board[7]))==(Character.valueOf(board[8])) && (Character.valueOf(board[8]))==(Character.valueOf(board[9])) && board[8]!=' ')
+            return board[7];
+        else if((Character.valueOf(board[1]))==(Character.valueOf(board[4])) && (Character.valueOf(board[4]))==(Character.valueOf(board[7])) && board[4]!=' ')
+            return board[1];
+        else if((Character.valueOf(board[2]))==(Character.valueOf(board[5])) && (Character.valueOf(board[5]))==(Character.valueOf(board[8])) && board[5]!=' ')
+            return board[2];
+        else if((Character.valueOf(board[3]))==(Character.valueOf(board[6])) && (Character.valueOf(board[6]))==(Character.valueOf(board[9])) && board[6]!=' ')
+            return board[3];
+        else if((Character.valueOf(board[1]))==(Character.valueOf(board[5])) && (Character.valueOf(board[5]))==(Character.valueOf(board[9])) && board[5]!=' ')
+            return board[1];
+        else if ((Character.valueOf(board[3]))==(Character.valueOf(board[5])) && (Character.valueOf(board[5]))==(Character.valueOf(board[7])) && board[5]!=' ')
+            return board[3];
+        else
+            return 'N';
+    }
     public static void main(String[] args) {
         System.out.println("     Welcome to TIC TAC TOE Game");
         System.out.println("****************************************");
@@ -87,24 +107,34 @@ public class TicTacToeGame {
             System.out.println("The TIC TAC TOE Board is Created!");
             showBoard();
             chooseLetter();
-            boolean win= Toss();
+            char nextPlayer= Toss();
             System.out.println("The Current state of the board is:");
             showBoard();
-            if(win)
-                lastPlayer=compLetter;
-            else
-                lastPlayer=choice;
-            for(int i=1;i<board.length;i++)
-                while(board[1]==' '||board[2]==' '||board[3]==' '||board[4]==' '||board[5]==' '||board[6]==' '||board[7]==' '||board[8]==' '||board[9]==' '){
-                        if(lastPlayer==choice){
-                            makeMove(compLetter);
-                            lastPlayer=compLetter;
-                        }
-                        else{
+            for(int i=1;i<board.length;i++) {
+                if (winner == 'N') {
+                    if (board[1] == ' ' || board[2] == ' ' || board[3] == ' ' || board[4] == ' ' || board[5] == ' ' || board[6] == ' ' || board[7] == ' ' || board[8] == ' ' || board[9] == ' ') {
+                        if (nextPlayer == choice) {
                             makeMove(choice);
-                            lastPlayer=choice;
+                            winner = checkWinner();
+                            nextPlayer = compLetter;
+                        } else {
+                            makeMove(compLetter);
+                            winner = checkWinner();
+                            nextPlayer = choice;
                         }
+                    }
                 }
+                else if(winner==choice) {
+                    System.out.println("Congratulations!!! You won the Game!!!");
+                    i=board.length;
+                }
+                else{
+                    System.out.println("Computer won the Game!!!\nBetter luck next time....");
+                    i=board.length;
+                }
+            }
+            if(winner== 'N')
+                System.out.println("Its a tie!!!\nBetter luck next time....");
         }
     }
 }
